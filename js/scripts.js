4c36e2a16bd67b9e109dbd8659ac984bddf5b766
Katarzyna Lachcik <kaisek@poczta.fm>
2017-07-02 21:44:53 +0100

    tabs

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 $(function(){
-	
+
 	var carouselList = $("#carousel ul");
 
 
-function changeSlides() {
+	function changeSlides() {
 	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-};
-
-function moveFirstSlide() {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-	}
+	};
 
 	setInterval(changeSlides, 3000);
+
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
 
 });
